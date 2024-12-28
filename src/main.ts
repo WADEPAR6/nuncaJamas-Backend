0619d50bc06f6878a40c80678f874a3d0086042d
WADEPAR6 <kparedes9203@uta.edu.ec>
2024-12-28 03:36:27 -0500

    FIX - Arreglo del peso de las solicitudes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,5 +9,8 @@ async function bootstrap() {
     origin: '*',
   });
   await app.listen(process.env.PORT ?? 3000);
+
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 }
 bootstrap();
